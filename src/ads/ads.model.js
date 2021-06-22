bf52d597fe464f1823e1a0e644e94d8770eedb8d
Lisnyk-M <swapbuffers@gmail.com>
2021-06-22 22:44:33 +0300

    first added

---
+++ src/ads/ads.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const adsSchema = Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: 1000,
+        trim: true,
+    },
+    links: [String],
+    date: String,
+})
+
+const adsModel = new mongoose.model('ads', adsSchema);
+
+module.exports = adsModel;
